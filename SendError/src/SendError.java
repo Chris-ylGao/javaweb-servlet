6848fbd1b8ffed6fbd9a02adfbc2df7c0aac60fa
Chris-ylGao <b9052278@newcastle.ac.uk>
2019-12-23 08:41:03 +0000

    recommit sendError project

---
+++ SendError/src/SendError.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class SendError extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.sendError(407, "Need Authentication!!!");
+        
+    }
+}
